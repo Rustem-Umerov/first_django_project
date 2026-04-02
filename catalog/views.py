643143a66a420b9721dd4e0aea47d69272f89fca
Rustem Umerov <rustem.umerov.00@yandex.ru>
2026-04-02 22:10:59 +0300

    Created a controller to display the home page and to display the contact information page.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,38 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request: HttpRequest) -> HttpResponse:
+    """
+    Отображает главную страницу приложения 'catalog'.
+
+    :param request: Объект HTTP-запроса.
+    :return: Ответ с отрендеренным шаблоном home.html.
+    """
+
+    return render(request=request, template_name="catalog/home.html")
+
+
+def contacts(request: HttpRequest) -> HttpResponse:
+    """
+    Отображает страницу с контактной информацией и POST‑форму.
+
+    :param request: Объект HTTP-запроса.
+    :return: Ответ с отрендеренным шаблоном contacts.html.
+    """
+
+    context = {}
+
+    if request.method == "POST":
+        name = request.POST.get("name", "").strip()
+        phone = request.POST.get("phone", "").strip()
+        message = request.POST.get("message", "").strip()
+
+        if not name or not phone or not message:
+            context["error_message"] = "Ошибка: заполните все поля"
+        else:
+            context["success_message"] = "Ваше сообщение успешно отправлено"
+
+    return render(
+        request=request, template_name="catalog/contacts.html", context=context
+    )
