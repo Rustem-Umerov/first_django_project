89db29a8dad10ef1535d0259ea810320b14985a1
Rustem Umerov <rustem.umerov.00@yandex.ru>
2026-04-15 13:38:07 +0300

    Registered the Product and Category models in the admin panel.

---
+++ catalog/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
+    list_display = ("id", "name")
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
+    list_display = ("id", "name", "price", "category")
+    list_filter = ("category",)
+    search_fields = ("name", "description")
+    list_display_links = ("name",)
