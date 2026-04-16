2a8a33bdd581a72de6045e075fb4f7719a58e310
Rustem Umerov <rustem.umerov.00@yandex.ru>
2026-04-16 18:34:46 +0300

    I added the following functions to views.py: * get_last_products() - Returns the last count of products, sorted by creation date. * print_products() - Outputs product information to the server console. I added a selection of the last 5 created products to the main page display controller and output them to the console.

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,35 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .models import Product
+
+
+def get_last_products(count: int) -> list[Product]:
+    """
+    Возвращает последние count продуктов, отсортированные по дате создания.
+
+    :param count: Количество элементов
+    :return: Список объектов Product
+    """
+
+    return list(Product.objects.order_by("-created_at")[:count])
+
+
+def print_products(products: list[Product]) -> None:
+    """
+    Выводит информацию о продуктах в консоль сервера.
+
+    :param products: Список объектов Product
+    """
+
+    for prod in products:
+        print(
+            f"Название продукта: {prod.name}, "
+            f"Цена: {prod.price}, "
+            f"Категория: {prod.category}, "
+            f"Дата создания: {prod.created_at}"
+        )
+
 
 def home(request: HttpRequest) -> HttpResponse:
     """
@@ -10,6 +39,8 @@ def home(request: HttpRequest) -> HttpResponse:
     :return: Ответ с отрендеренным шаблоном home.html.
     """
 
+    products = get_last_products(5)
+    print_products(products)
     return render(request=request, template_name="catalog/home.html")
 
 
