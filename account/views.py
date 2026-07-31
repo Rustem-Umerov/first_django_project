55e92696a2d163d34a4cd63bc5687d57f25df7b1
Rustem Umerov <rustem.umerov.00@yandex.ru>
2026-07-31 10:50:33 +0300

    * In the templates/includes/global_navbar.html template, I specified the "next" parameter for the "Logout" button so that the redirect would be to the public part of the site. * In account/views.py, in the logout_and_stay() function, I wrote code that handles the "next" parameter.

--- account/views.py
+++ account/views.py
@@ -12,7 +12,7 @@ def logout_and_stay(request: HttpRequest) -> HttpResponse:
     Поведение:
     - выполняет logout()
     - добавляет сообщение об успешном выходе
-    - делает redirect обратно на страницу-источник (HTTP_REFERER)
+    - делает redirect на next_url, если он есть, иначе делает redirect обратно на страницу-источник (HTTP_REFERER)
       или на главную страницу, если источник неизвестен
 
     :param request: объект HttpRequest текущего пользователя
@@ -21,4 +21,9 @@ def logout_and_stay(request: HttpRequest) -> HttpResponse:
 
     logout(request)
     messages.success(request, "Вы вышли из аккаунта")
+
+    next_url = request.GET.get("next")
+    if next_url:
+        return redirect(next_url)
+
     return redirect(request.META.get("HTTP_REFERER", "/"))
