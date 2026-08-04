cecd340a03838e01e410fc97f06417818f32b453
Rustem Umerov <rustem.umerov.00@yandex.ru>
2026-08-04 17:25:38 +0300

    * Overridden the get_object() method in the BlogPostDetailView controller. Added view history. * In templates: blog/templates/blog/account/my_post_detail.html blog/templates/blog/account/my_published.html blog/templates/blog/public/post_detail.html blog/templates/blog/public/post_list.html added information about the number of post views.

--- blog/views/public.py
+++ blog/views/public.py
@@ -1,7 +1,10 @@
+from typing import Optional
+
 from django.db.models import Q, QuerySet
+from django.utils import timezone
 from django.views.generic import DetailView, ListView
 
-from ..models import BlogPost
+from ..models import BlogPost, PostView
 
 
 class BlogPostListView(ListView):  # type: ignore[type-arg]
@@ -39,3 +42,44 @@ class BlogPostDetailView(DetailView):  # type: ignore[type-arg]
     model = BlogPost
     template_name = "blog/public/post_detail.html"
     context_object_name = "post"
+
+    def get_object(self, queryset: Optional[QuerySet[BlogPost]] = None) -> BlogPost:
+        """
+        Переопределен метод.
+        Добавлена история просмотра для пользователя (авторизованного/неавторизованного)
+        Если пользователь авторизован, то мы работаем с базой данных PostView
+        Если не авторизован - работаем с сессией.
+        """
+
+        post: BlogPost = super().get_object()
+        user = self.request.user
+
+        # --- Авторизованный пользователь ---
+        # Если пользователь авторизован, то мы работаем с моделью PostView
+        if user.is_authenticated:
+            # Проверяем смотрел ли данный пост текущий пользователь
+            post_view = PostView.objects.filter(post=post, user=user)
+            if post_view.exists():
+                # Если есть совпадения, то, просто, обновляем дату просмотра
+                post_view.update(viewed_at=timezone.now())
+
+            else:
+                # Если совпадений нет, создаем запись в базе PostView
+                PostView.objects.create(post=post, user=user)
+                post.count_views += 1  # Увеличиваем счетчик у поста (объект BlogPost)
+                post.save()  # Сохраняем
+
+        # --- Неавторизованный пользователь ---
+        else:
+            # Получаем объект сессии текущего пользователя
+            viewed_posts = self.request.session.get("viewed_posts", [])
+
+            # Если гость ещё не смотрел этот пост, добавляем его pk в список
+            if post.pk not in viewed_posts:
+                viewed_posts.append(post.pk)
+                self.request.session["viewed_posts"] = viewed_posts
+
+                post.count_views += 1  # Увеличиваем счетчик у поста (объект BlogPost)
+                post.save()  # Сохраняем
+
+        return post  # Возвращаем объект поста
