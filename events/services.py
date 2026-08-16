8724d728807de094dc09df0affc60d80e28eefb0
Rustem Umerov <rustem.umerov.00@yandex.ru>
2026-08-16 19:46:56 +0300

    I wrote the handle_post_view_event() function to write an event to the Event table.

---
+++ events/services.py
@@ -0,0 +1,37 @@
+from django.contrib.contenttypes.models import ContentType
+
+from blog.models import BlogPost
+
+from .enums import EventType
+from .models import Event
+
+
+def handle_post_view_event(post: BlogPost) -> bool:
+    """
+    Проверяет количество просмотров у поста.
+    Проверяет не записано ли это событие в таблице Event.
+    Если количество просмотров равно 100 и записи в таблице Event нет,
+    то делает запись в таблицу Event.
+
+    :param post: Объект поста (класс BlogPost).
+    :return: Если событие было создано - True, иначе - False.
+    """
+
+    view_count = post.count_views
+
+    if view_count != 100:
+        return False
+
+    if Event.objects.filter(
+        content_type=ContentType.objects.get_for_model(post),
+        object_id=post.pk,
+        event_type=EventType.POST_REACHED_100_VIEWS.value,
+    ).exists():
+        return False
+
+    Event.objects.create(
+        content_type=ContentType.objects.get_for_model(post),
+        object_id=post.pk,
+        event_type=EventType.POST_REACHED_100_VIEWS.value,
+    )
+    return True
