acac257e2cf4f4b581b5c03453ceda3fac5ebaae
Rustem Umerov <rustem.umerov.00@yandex.ru>
2026-09-08 16:26:59 +0300

    In the catalog/forms.py module, I added the following methods to the ProductForm form: * clean_name() - Checks for prohibited words in the 'name' field * clean_description() - Checks for prohibited words in the 'description' field

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,6 +1,21 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+from core.validators import validate_bad_words
 
 from .models import Product
+
+BAD_WORD = [
+    "казино",
+    "криптовалюта",
+    "крипта",
+    "биржа",
+    "дешево",
+    "бесплатно",
+    "обман",
+    "полиция",
+    "радар",
+]
 
 
 class ProductForm(forms.ModelForm):
@@ -33,3 +48,33 @@ class ProductForm(forms.ModelForm):
                 attrs={"id": "id_price", "class": "form-control"}
             ),
         }
+
+    def clean_name(self) -> str:
+        """Проверяет наличие запрещенных слов в поле 'name'"""
+
+        name = self.cleaned_data.get("name")
+        if name is None:
+            raise ValidationError("Поле 'name' не прошло проверку.")
+
+        bool_value, bad_word = validate_bad_words(value=name, bad_words=BAD_WORD)
+
+        if bool_value:
+            raise ValidationError(f"В поле 'name' есть запрещенное слово '{bad_word}'.")
+
+        return name
+
+    def clean_description(self) -> str:
+        """Проверяет наличие запрещенных слов в поле 'description'"""
+
+        description = self.cleaned_data.get("description")
+        if description is None:
+            raise ValidationError("Поле 'description' не прошло проверку.")
+
+        bool_value, bad_word = validate_bad_words(value=description, bad_words=BAD_WORD)
+
+        if bool_value:
+            raise ValidationError(
+                f"В поле 'description' есть запрещенное слово '{bad_word}'."
+            )
+
+        return description
