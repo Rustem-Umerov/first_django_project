c967dfb0931d2f24a45c09520c358c33b5451e16
Rustem Umerov <rustem.umerov.00@yandex.ru>
2026-09-10 13:23:52 +0300

    In blog/admin.py in BlogPostAdmin, I added the method: * save_model() - for properly saving the post photo.

--- blog/admin.py
+++ blog/admin.py
@@ -1,17 +1,55 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.http import HttpRequest
 
+from .forms import BlogPostForm
 from .models import BlogPost, Profile
 from .typing import UserWithProfile
 
 
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
+    form = BlogPostForm
     list_display = ("author", "title", "is_published", "created_at", "count_views")
     list_filter = ("author", "is_published", "created_at")
     search_fields = ("title", "content")
     ordering = ("-created_at",)
+
+    def save_model(
+        self, request: HttpRequest, obj: BlogPost, form: BlogPostForm, change: bool
+    ) -> None:
+        """
+        Переопределение метода, для того чтобы сначала сохранить объект без фото, если оно есть.
+        Для чего это нужно? Дело в том, что фото объекта должно быть сохранено в папку с названием номера pk объекта.
+        Проблема в том, что если объект новый, то во время его создания pk еще нет,
+        pk появится уже после создания объекта.
+        Поэтому мы сохраняем объект без фото. Потом, когда pk появится, мы добавляем в объект фото.
+
+        :param request: Текущий HTTP‑запрос из админки
+        :param obj: экземпляр модели BlogPost, который сохраняется
+        :param form: форма, содержащая обработанные данные и cleaned_data
+        :param change: флаг, показывающий, редактируется объект или создаётся впервые
+        :return: None
+        """
+
+        # Если объект новый (создаётся впервые)
+        if not obj.pk:
+            # Забираем файл из формы
+            image = form.cleaned_data.get("image")
+
+            # Временно убираем файл с объекта
+            obj.image = None
+
+            # Сначала сохраняем объект без файла и появится pk
+            super().save_model(request, obj, form, change)
+
+            if image:
+                obj.image = image
+                obj.save()
+        else:
+            # Если объект редактируется — обычное сохранение
+            super().save_model(request, obj, form, change)
 
 
 # ==================================================================
