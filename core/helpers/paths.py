0dc85278178ae8960871b89734fe7264b8bae7cb
Rustem Umerov <rustem.umerov.00@yandex.ru>
2026-09-10 10:53:34 +0300

    In the core/helpers/paths.py module, in the get_folder_path() function, I renamed the "field_name" parameter to "field_file" because the function works with a file object.

--- core/helpers/paths.py
+++ core/helpers/paths.py
@@ -4,15 +4,15 @@ from typing import Optional
 from django.db.models.fields.files import FieldFile
 
 
-def get_folder_path(field_name: FieldFile) -> Optional[Path]:
+def get_folder_path(field_file: FieldFile) -> Optional[Path]:
     """
     Возвращает путь к папке, в которой лежит файл.
 
-    :param field_name: Объект файла
+    :param field_file: Объект файла
     """
 
     try:
-        file_path = Path(field_name.path)
+        file_path = Path(field_file.path)
     except (ValueError, TypeError, FileNotFoundError):
         return None
 
